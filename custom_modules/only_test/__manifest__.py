4ead50f994313388709805b04fc2cc61554a486f
Эрдэнэхүү Энхээ <erdenekhuu.e@gmobile.mn>
2025-06-27 18:24:16 +0800

    .

--- custom_modules/only_test/__manifest__.py
+++
@@ -1,9 +0,0 @@
-{
-    'name': 'Only Test Module',
-    'version': '1.0',
-    'summary': 'Extended from mail.mail',
-    'description': "Extends the mail module to add custom functionality",
-    'depends': ['mail'],
-    'installable': True,
-    'application': True,
-}
