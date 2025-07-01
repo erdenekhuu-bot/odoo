fd9ebdd8c43ae74df7af98e163bb5aa37b73eb6f
Эрдэнэхүү Энхээ <erdenekhuu.e@gmobile.mn>
2025-07-01 16:01:43 +0800

    .

--- custom_modules/extend_contract/__manifest__.py
+++ custom_modules/extend_contract/__manifest__.py
@@ -4,6 +4,10 @@
     'description': 'mail.list, mail.contact oroltuulah',
     'author': 'admin',
     'depends': ['base', 'mass_mailing'],
+    'version': '1.0',
+    'data': [
+        'views/ui.xml'
+    ],
     'application': True,
     'installable': True
 }
--- custom_modules/extend_contract/models/attach_mail_contact.py
+++ custom_modules/extend_contract/models/attach_mail_contact.py
@@ -1,6 +1,5 @@
 from odoo import models, fields
 
 class AttachMailCustom(models.Model):
-    _inherit='mail.mail'
-    mailing_list_id=fields.Many2one('mailing.list', string='Mailing List')
-    mailing_contact_id=fields.Many2one('mailing.contact', string='Mailing Contact')
+    _inherit='mailing.list'
+    email=fields.Char(string='email')  
