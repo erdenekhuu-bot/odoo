fc51d0f3ae40c1d8ec2c0dce5a29422ecc6f1dc0
Эрдэнэхүү Энхээ <erdenekhuu.e@gmobile.mn>
2025-08-19 18:28:48 +0800

    'thgouth_graph_api'

--- custom_modules/graph_outlook/models/mailing.py
+++ custom_modules/graph_outlook/models/mailing.py
@@ -6,7 +6,7 @@ import os
 _logger = logging.getLogger(__name__)
 
 # sender_email = 'no-reply@gmobile.mn'
-sender_email = 'alert@gmobile.mn'
+sender_email = 'newsletter@gmobile.mn'
 
 class MailingMailing(models.Model):
     _inherit = 'mailing.mailing'
@@ -21,29 +21,6 @@ class MailingMailing(models.Model):
         _logger.info(f"**************** ACTION TRIGGERED **************************")
         return super().action_launch()
     
-
-    def action_test10(self):
-        token=get_ms_token()
-       
-        _logger.info(f"Access token acquired successfully")
-
-        for contact in self.mailing.mailing.search(['subject']):
-            to_email=contact.email
-            subject=self.subject
-            body_html=self.body_html
-            if not to_email:
-                continue 
-            _logger.info(f"Preparing to send email to {subject}")
-
-            status, message= send_ms_email(token, self.from_email, to_email, subject, body_html)
-            _logger.info(f"Email sent to {to_email} with status {status}")
-            if status >= 400:
-                _logger.error(f"Failed to send email to {to_email}: {message}")
-            else:
-                _logger.info(f"Email sent successfully to {to_email}")
-
-        return True
-
     
     def _action_send_mail(self, res_ids=None):
         token = get_ms_token()
