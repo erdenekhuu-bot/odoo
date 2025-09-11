642856e8113370f8cc492fa56b48cd2cbd087a41
Эрдэнэхүү Энхээ <erdenekhuu.e@gmobile.mn>
2025-09-11 09:42:30 +0800

    'changed_display_email'

--- custom_modules/extend_contract/models/attach_mail_contact.py
+++ custom_modules/extend_contract/models/attach_mail_contact.py
@@ -7,15 +7,6 @@ class ConstMail(models.Model):
     @api.model
     def default_get(self, fields):
         res = super().default_get(fields)
-        res['email_from'] = 'NewsLetter <alert@gmobile.mn>'
-        res['reply_to'] = 'NewsLetter <alert@gmobile.mn>'
+        res['email_from'] = 'Gmobile NewsLetter <alert@gmobile.mn>'
+        res['reply_to'] = 'Gmobile NewsLetter <alert@gmobile.mn>'
         return res
-
-class IrConfigParameter(models.Model):
-    _inherit = 'ir.config_parameter'
-
-    @api.model
-    def get_param(self, key, default=False):
-        if key == 'web.base.url':
-            return 'https://massmail.gmobile.mn'  
-        return super().get_param(key, default)
