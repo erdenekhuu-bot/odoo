4e9b88c8f229ee02f300e7cc4bed100833eaacad
erdenekhuu.e <erdenekhuu.e@gmobile.mn>
2026-06-03 16:25:39 +0800

    .

--- custom_modules/invoice_own/models/read_billing.py
+++ custom_modules/invoice_own/models/read_billing.py
@@ -117,7 +117,7 @@ class ReadBilling(models.Model):
             'logo_b64': logo_b64,
             'app_b64': app_b64,
             'qr_b64': qr_b64,
-            'lang': 'mn_MN',
+            'lang': 'en_US',
         }
 
         # html_content = self.env['ir.qweb']._render(
