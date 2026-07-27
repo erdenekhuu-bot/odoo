c2d08286aee4fce183c6f371099c02ab7a828978
erdenekhuu.e <erdenekhuu.e@gmobile.mn>
2026-07-27 11:56:56 +0800

    check.view

--- custom_modules/business_company/model/read_billing.py
+++ custom_modules/business_company/model/read_billing.py
@@ -499,7 +499,7 @@ class ReadBilling(models.Model):
                         continue
 
                     # Тухайн account-ийн email-ийг billing.group-оос олно
-                    group = BillingGroup.search([],limit=1)
+                    group = BillingGroup.search([])
                     if not group:
                         skipped_count += 1
 
