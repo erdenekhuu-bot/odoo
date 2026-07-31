a832051863906146045c28be503b35e7e00cf35c
erdenekhuu.e <erdenekhuu.e@gmobile.mn>
2026-07-31 11:09:05 +0800

    customized.again

--- custom_modules/business_company/model/read_billing.py
+++ custom_modules/business_company/model/read_billing.py
@@ -225,8 +225,8 @@ class ReadBilling(models.Model):
         ], limit=1)
 
         head_data = self.generate_head_data([bills.own_network_limit, bills.other_call_limit, bills.all_call_limit, bills.data_limit, bills.sms_limit], tagged_types)
-        #data = [i for i in self.filter_items(bills.bill_items or [], selected_types, new_label) if i['amount'] > 0]
-        data=self.filter_items(bills.bill_items or [], selected_types, new_label)
+        data = [i for i in self.filter_items(bills.bill_items or [], selected_types, new_label) if i['amount'] > 0]
+        # data=self.filter_items(bills.bill_items or [], selected_types, new_label)
         date_obj = datetime.strptime(start_date, '%Y-%m-%d')
         year = date_obj.year
         month = date_obj.month
