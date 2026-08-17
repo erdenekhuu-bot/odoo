11d443e5447127ad5a8073360b39bd8f2e8362f6
erdenekhuu.e <erdenekhuu.e@gmobile.mn>
2026-08-17 14:00:43 +0800

    hide.useless.column.sms

--- custom_modules/sms_gmobile/__manifest__.py
+++ custom_modules/sms_gmobile/__manifest__.py
@@ -7,6 +7,9 @@
     'summary': 'SMS broadcasting to customers',
     'depends': ['base','web','website','sms','mass_mailing_sms','sms'],
     'author': 'erdenekhuu.e',
+    'data':[
+        'view/menu.xml',
+    ],
     'installable': True,
     'application': True,
 }
