6b3165ac7612ffe3ab27f953a1023d371624aa8e
erdenekhuu.e <erdenekhuu.e@gmobile.mn>
2026-09-18 14:45:38 +0800

    interact.mail.thread.back

--- custom_modules/gmobile_incoming_reply/models/incoming_reply.py
+++ custom_modules/gmobile_incoming_reply/models/incoming_reply.py
@@ -9,7 +9,7 @@ import smtplib
 
 class IncomingReply(models.Model):
     _name = 'gmobile.incoming.reply'
-    # _inherit = ['mail.thread']
+    _inherit = ['mail.thread']
     _description = 'Incoming Reply Log'
     _rec_name = 'subject'
     _order = 'received_date desc'
@@ -20,19 +20,17 @@ class IncomingReply(models.Model):
     received_date = fields.Datetime(string="Received", default=fields.Datetime.now)
     reply_body = fields.Html(string="Reply Text")
 
-
-
-    # @api.model
-    # def message_new(self, msg_dict, custom_values=None):
-    #     values = {
-    #         'email_from': msg_dict.get('from'),
-    #         'subject': msg_dict.get('subject'),
-    #         'body_preview': msg_dict.get('body'),
-    #         'received_date': fields.Datetime.now(),
-    #     }
-    #     if custom_values:
-    #         values.update(custom_values)
-    #     return super().message_new(msg_dict, values)
+    @api.model
+    def message_new(self, msg_dict, custom_values=None):
+        values = {
+            'email_from': msg_dict.get('from'),
+            'subject': msg_dict.get('subject'),
+            'body_preview': msg_dict.get('body'),
+            'received_date': fields.Datetime.now(),
+        }
+        if custom_values:
+            values.update(custom_values)
+        return self.create(values)
 
     # def action_reply_to_customer(self):
     #     self.ensure_one()
