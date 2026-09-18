482cb9175aac68752c309a2a73a050d2bc1cf297
erdenekhuu.e <erdenekhuu.e@gmobile.mn>
2026-09-18 14:28:40 +0800

    mail.thread.back

--- custom_modules/gmobile_incoming_reply/models/incoming_reply.py
+++ custom_modules/gmobile_incoming_reply/models/incoming_reply.py
@@ -1,9 +1,11 @@
 from odoo import models, fields, api
 from odoo.exceptions import UserError
+from email import message_from_bytes
+from email.header import decode_header
+from email.message import EmailMessage
+from email.utils import parseaddr
+import imaplib
 import smtplib
-import os
-from email.mime.text import MIMEText
-from email.mime.multipart import MIMEMultipart
 
 class IncomingReply(models.Model):
     _name = 'gmobile.incoming.reply'
@@ -18,34 +20,7 @@ class IncomingReply(models.Model):
     received_date = fields.Datetime(string="Received", default=fields.Datetime.now)
     reply_body = fields.Html(string="Reply Text")
 
-    def action_back_reply(self):
-        self.ensure_one()
-        config = self.env["ir.config_parameter"].sudo()
-        if not self.email_from:
-            raise UserError("No recipient email address found.")
-        if not self.reply_body:
-            raise UserError("Please write a reply before sending.")
 
-        smtp_user = config.get_param("smtp.user")
-        smtp_pass = config.get_param("smtp.pass")
-        if not smtp_user or not smtp_pass:
-            raise UserError("No SMTP credentials configured.")
-
-        msg = MIMEMultipart()
-        msg['From'] = smtp_user
-        msg['To'] = self.email_from
-        msg['Subject'] = f"Re: {self.subject or ''}"
-        msg.add_header('Reply-To', 'alert@gmobile.mn')
-        msg.attach(MIMEText(self.reply_body, 'html'))
-
-        try:
-            smtp_server = smtplib.SMTP('mail.gmobile.mn', 587)
-            smtp_server.starttls()
-            smtp_server.login(smtp_user, smtp_pass)
-            smtp_server.sendmail(smtp_user, [self.email_from], msg.as_string())
-            smtp_server.quit()
-        except smtplib.SMTPException as e:
-            raise UserError(str(e))
 
     # @api.model
     # def message_new(self, msg_dict, custom_values=None):
