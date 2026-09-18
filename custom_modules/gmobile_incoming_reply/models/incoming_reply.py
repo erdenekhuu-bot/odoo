d39d24c12b7945e0c58610b99a8815588db87203
erdenekhuu.e <erdenekhuu.e@gmobile.mn>
2026-09-18 10:59:26 +0800

    reply.back.with.server.action

--- custom_modules/gmobile_incoming_reply/models/incoming_reply.py
+++ custom_modules/gmobile_incoming_reply/models/incoming_reply.py
@@ -1,5 +1,9 @@
 from odoo import models, fields, api
 from odoo.exceptions import UserError
+import smtplib
+import os
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
 
 class IncomingReply(models.Model):
     _name = 'gmobile.incoming.reply'
@@ -14,6 +18,35 @@ class IncomingReply(models.Model):
     received_date = fields.Datetime(string="Received", default=fields.Datetime.now)
     reply_body = fields.Html(string="Reply Text")
 
+    def action_back_reply(self):
+        self.ensure_one()
+        config = self.env["ir.config_parameter"].sudo()
+        if not self.email_from:
+            raise UserError("No recipient email address found.")
+        if not self.reply_body:
+            raise UserError("Please write a reply before sending.")
+
+        smtp_user = config.get_param("smtp.user")
+        smtp_pass = config.get_param("smtp.pass")
+        if not smtp_user or not smtp_pass:
+            raise UserError("No SMTP credentials configured.")
+
+        msg = MIMEMultipart()
+        msg['From'] = smtp_user
+        msg['To'] = self.email_from
+        msg['Subject'] = f"Re: {self.subject or ''}"
+        msg.add_header('Reply-To', 'alert@gmobile.mn')
+        msg.attach(MIMEText(self.reply_body, 'html'))
+
+        try:
+            smtp_server = smtplib.SMTP('mail.gmobile.mn', 587)
+            smtp_server.starttls()
+            smtp_server.login(smtp_user, smtp_pass)
+            smtp_server.sendmail(smtp_user, [self.email_from], msg.as_string())
+            smtp_server.quit()
+        except smtplib.SMTPException as e:
+            raise UserError(str(e))
+
     # @api.model
     # def message_new(self, msg_dict, custom_values=None):
     #     values = {
@@ -26,31 +59,31 @@ class IncomingReply(models.Model):
     #         values.update(custom_values)
     #     return super().message_new(msg_dict, values)
 
-    def action_reply_to_customer(self):
-        self.ensure_one()
-        config = self.env["ir.config_parameter"].sudo()
-        if not self.email_from:
-            raise UserError("No customer email address")
-        if not self.reply_body:
-            raise UserError("Please write a reply before sending.")
-
-        mail = self.env['mail.mail'].sudo().create({
-            'email_from': config.get_param("main.mail"),
-            'email_to': self.email_from,
-            'subject': f"Re: {self.subject or ''}",
-            'body_html': self.reply_body,
-            'model': self._name,
-            'res_id': self.id,
-            'auto_delete': True,
-            'state': 'outgoing',
-        })
-        return {
-            'name': 'Reply back',
-            'type': 'ir.actions.act_window',
-            'res_model': 'mail.mail',
-            'res_id': mail.id,
-            'view_mode': 'form',
-            'view_id': self.env.ref('mail.view_mail_form').id,
-            'target': 'new',
-            'context': {'create': False},
-        }
+    # def action_reply_to_customer(self):
+    #     self.ensure_one()
+    #     config = self.env["ir.config_parameter"].sudo()
+    #     if not self.email_from:
+    #         raise UserError("No customer email address")
+    #     if not self.reply_body:
+    #         raise UserError("Please write a reply before sending.")
+    #
+    #     mail = self.env['mail.mail'].sudo().create({
+    #         'email_from': config.get_param("main.mail"),
+    #         'email_to': self.email_from,
+    #         'subject': f"Re: {self.subject or ''}",
+    #         'body_html': self.reply_body,
+    #         'model': self._name,
+    #         'res_id': self.id,
+    #         'auto_delete': True,
+    #         'state': 'outgoing',
+    #     })
+    #     return {
+    #         'name': 'Reply back',
+    #         'type': 'ir.actions.act_window',
+    #         'res_model': 'mail.mail',
+    #         'res_id': mail.id,
+    #         'view_mode': 'form',
+    #         'view_id': self.env.ref('mail.view_mail_form').id,
+    #         'target': 'new',
+    #         'context': {'create': False},
+    #     }
